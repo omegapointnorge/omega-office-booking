d35f81f3f22e233c7ab18abb5b1f2872d1adcfdf
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-10-31 08:29:20 +0100

    Test files for authentication cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import LoginPage from "./loginpage";
-import Overview from "./overview";
+import logo from './logo.svg';
+import './App.css';
 
 export default function App() {
-  return (
-        <Routes>
-            <Route path="/" element={<LoginPage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="/overview" element={<Overview />}></Route>
-        </Routes>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>)
 }
 App.displayName = App.name
