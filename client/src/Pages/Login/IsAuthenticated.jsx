eb0d57a21d4ae15b99b20cdeb2b0e17988c0f24b
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-08 14:07:47 +0100

    Removed not needed dependency array

--- client/src/Pages/Login/IsAuthenticated.jsx
+++ client/src/Pages/Login/IsAuthenticated.jsx
@@ -1,24 +1,23 @@
 import { CircularProgress } from "@mui/material";
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import {useAuthContext} from "../../api/useAuthContext";
+import { useAuthContext } from "../../api/useAuthContext";
 export const IsAuthenticated = () => {
   const navigate = useNavigate();
   const context = useAuthContext();
-  const isMounted = useRef(false)
-    useEffect(() => {
-        isMounted.current = true;
-        if (context?.user?.isAuthenticated) {
-            console.log('User is authenticated, redirecting to overview..')
-            isMounted.current = false;
-            navigate("/overview");
-        }else{
-            isMounted.current = false;
-            navigate("/login");
-        }
-    }, [navigate]);
-    
-    
+  const isMounted = useRef(false);
+  useEffect(() => {
+    isMounted.current = true;
+    if (context?.user?.isAuthenticated) {
+      console.log("User is authenticated, redirecting to overview..");
+      isMounted.current = false;
+      navigate("/overview");
+    } else {
+      isMounted.current = false;
+      navigate("/login");
+    }
+  });
+
   return (
     <div className="flex flex-col">
       <CircularProgress size={100} style={{ margin: "10vh auto" }} />
