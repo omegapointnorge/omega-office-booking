40b2da6c558a1222c7c25699ea65ff43d019d2b3
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-08 10:59:14 +0100

    Exported Routes to own file

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginPage from './Pages/Login/LoginPage'
-import  {Route, Routes} from 'react-router-dom';
-import OverviewPage from "./Pages/Overview/OverviewPage";
-import {IsAuthenticated} from "./Pages/Login/IsAuthenticated";
+import Routers from "./Routes/Routers";
+
+import React from "react";
 export default function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<IsAuthenticated />}></Route>
-                <Route path="/login" element={<LoginPage />}></Route>
-                <Route path="/overview" element={<OverviewPage />}></Route>
-            </Routes>
+            <Routers />
         </div>)
 }
 App.displayName = App.name
---
+++ client/src/Routes/Routers.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import {IsAuthenticated} from "../Pages/Login/IsAuthenticated";
+import LoginPage from "../Pages/Login/LoginPage";
+import OverviewPage from "../Pages/Overview/OverviewPage";
+
+
+const Routers = () => {
+    return (<Routes>
+        <Route path="/" element={<IsAuthenticated />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+
+        {/* PROTECTED ROUTES */}
+        <Route path="/overview" element={<OverviewPage />}></Route>
+    </Routes>)
+}
+
+export default Routers;
