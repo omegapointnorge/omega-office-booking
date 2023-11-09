9631108c61b0b0f27e39c59ade360453a17fa64d
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-09 08:48:22 +0100

    Removed not needed z-index

--- client/src/Pages/Overview/OverviewPage.jsx
+++ client/src/Pages/Overview/OverviewPage.jsx
@@ -11,7 +11,7 @@ export default function OverviewPage() {
     return (
         <>
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto 
-     fixed inset-0 z-50 outline-none focus:outline-none" >
+     fixed inset-0 outline-none focus:outline-none" >
                 <Heading title="Overview page" subTitle={`Velkommen ${name}`}/>
                 
             </div>
