d4bdcdf3b51043e2fa64b88879228fb57868a134
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-09 08:56:19 +0100

    Added new LogoutButton.jsx Added new Logo with OmegaPoint logo

--- client/src/Pages/Overview/OverviewPage.jsx
+++ client/src/Pages/Overview/OverviewPage.jsx
@@ -13,7 +13,6 @@ export default function OverviewPage() {
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto 
      fixed inset-0 outline-none focus:outline-none" >
                 <Heading title="Overview page" subTitle={`Velkommen ${name}`}/>
-                
             </div>
         </>)
 }
