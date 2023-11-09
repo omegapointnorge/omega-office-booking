93fc4d0a1a601814c2c64ddabbfe8e2599ffb704
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-09 14:27:14 +0100

    Added Navbar with Navigation

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,24 @@
 import {Logo} from "./Logo";
 import {LogoutButton} from "../Buttons/LogoutButton";
+import {useNavigate} from "react-router-dom";
+import NavbarItem from "./NavbarItem"
 
-type NavbarProps = {
-    userName?: string | null; 
-}
 
-export const Navbar = ({userName} : NavbarProps) => {
+export const Navbar = () => {
+    const navigate = useNavigate();
+    
     return (
         <div className="fixed w-full bg-white z-10 shadow-sm ">
             <div className="py-4 border-b-[1px]">
                 <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
-                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0 cursor-pointer">
                         <Logo />
+                        <NavbarItem label="Overview" onClick={() => {
+                            navigate("/overview");
+                        }} />
+                        <NavbarItem label="Rooms" onClick={() => {
+                            navigate("/rooms");
+                        }} />
                         <LogoutButton />
                     </div>
                 </div>
