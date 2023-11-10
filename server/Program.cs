695394ba249b3ac59c202adefa55348d5edae35a
GmailGit <vicky.huangyuanxin@gmail.com>
2023-11-10 11:14:27 +0100

    commit with cookies name change

--- server/Program.cs
+++ server/Program.cs
@@ -10,7 +10,6 @@ using server.Context;
 using server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Set our ClientId and ClientSecret in appsettings to user-secrets values
 builder.Configuration["AzureAD:ClientId"] = builder.Configuration["AzureAd__ClientId"];
 builder.Configuration["AzureAD:ClientSecret"] = builder.Configuration["AzureAd__ClientSecret"];
@@ -18,110 +17,47 @@ builder.Configuration["AzureAD:TenantId"] = builder.Configuration["AzureAd__Tena
 
 
 // Add services to the container.
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-})
-.AddMicrosoftIdentityWebApp(options =>
-{
-    builder.Configuration.Bind("AzureAd", options);
-    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.ResponseType = OpenIdConnectResponseType.Code;
-}, cookieOptions =>
-{
-    cookieOptions.ExpireTimeSpan = TimeSpan.FromHours(1);
-    cookieOptions.SlidingExpiration = true;
-    cookieOptions.Cookie.HttpOnly = true;
-    cookieOptions.Cookie.IsEssential = true;
-    cookieOptions.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    cookieOptions.Cookie.SameSite = SameSiteMode.Strict;
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+//builder.Services.AddCors();
 
-    cookieOptions.Events.OnRedirectToAccessDenied = c =>
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+
+    .AddCookie(o =>
     {
-        c.Response.StatusCode = StatusCodes.Status403Forbidden;
-        return Task.CompletedTask;
-    };
-    cookieOptions.Events.OnRedirectToLogin = c =>
-    {
-        c.Response.StatusCode = StatusCodes.Status401Unauthorized;
-        return Task.CompletedTask;
-    };
-});
+        o.Cookie.Name = "__bookingapp";
+        o.Cookie.SameSite = SameSiteMode.Strict;
+        o.Events.OnRedirectToLogin = c =>
+        {
+            c.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            return Task.CompletedTask;
+        };
+    });
 builder.Services.AddAuthorization(options =>
 {
     var defaultPolicy = new AuthorizationPolicyBuilder()
         .RequireAuthenticatedUser()
         .Build();
 
-    options.AddPolicy("AuthenticatedUser", defaultPolicy);
-    options.DefaultPolicy = defaultPolicy;
-    options.FallbackPolicy = defaultPolicy;
 });
-builder.Services.AddControllersWithViews();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-
-
-
-//builder.Services.AddDbContext<OfficeDbContext>(options => {
-//    SqlAuthenticationProvider.SetProvider(
-//        SqlAuthenticationMethod.ActiveDirectoryManagedIdentity,
-//        new server.Helpers.AzureSqlAuthProvider());
-
-//    var connectionString = "Server=tcp:" + builder.Configuration["SqlServerName"] + 
-//                           ".database.windows.net;Database=" + builder.Configuration["SqlDatabaseName"] +
-//                           ";TrustServerCertificate=True;Authentication=Active Directory Default";
-//    var sqlConnection = new SqlConnection(connectionString);
-//    options.UseSqlServer(sqlConnection);
-//});
-
-//TODO: change to sqldatabase in the end
-//builder.Services.AddDbContext<OfficeDbContext>(options => {
-//    SqlAuthenticationProvider.SetProvider(
-//        SqlAuthenticationMethod.ActiveDirectoryManagedIdentity,
-//        new server.Helpers.AzureSqlAuthProvider());
-
-//    var connectionString = "Server=tcp:" + builder.Configuration["SqlServerName"] +
-//                           ".database.windows.net;Database=" + builder.Configuration["SqlDatabaseName"] +
-//                           ";TrustServerCertificate=True;Authentication=Active Directory Default";
-//    var sqlConnection = new SqlConnection(connectionString);
-//    options.UseSqlServer(sqlConnection);
-//});
 builder.Services.AddDbContext<OfficeDbContext>(options => options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 builder.Services.AddScoped<ISeatRepository, SeatRepository>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-//builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseCookiePolicy();
-
 app.UseStaticFiles();
+app.UseAuthentication();
+
+//app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+
 app.MapSeatEndpoints();
 app.UseRouting();
-//after routing before authentication
-//app.UseCors(p => p.WithOrigins("https://appmodelv2-webapp-openidconnect-dotnet20231017.azurewebsites.net", "https://localhost:5001")
-//            .AllowAnyHeader()
-//            .AllowAnyMethod()
-//            .AllowCredentials());
-// Authentication middleware
-app.UseAuthentication();
 app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller}/{action=Index}/{id?}");
 app.UseEndpoints(e => e.MapDefaultControllerRoute());
 app.MapFallbackToFile("index.html");
 
