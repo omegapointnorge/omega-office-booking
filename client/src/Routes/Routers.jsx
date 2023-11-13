986bfd0d36abe021793c3bc1459c85bcbf35e5a8
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-13 11:09:12 +0100

    Fixed some warnings Removed BigRoomPage.jsx

--- client/src/Routes/Routers.jsx
+++ client/src/Routes/Routers.jsx
@@ -1,24 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import {IsAuthenticated} from "../Pages/Login/IsAuthenticated";
+import { IsAuthenticated } from "../Pages/Login/IsAuthenticated";
 import LoginPage from "../Pages/Login/LoginPage";
 import OverviewPage from "../Pages/Overview/OverviewPage";
-import {ProtectedRoute} from "./ProtectedRoute";
+import { ProtectedRoute } from "./ProtectedRoute";
 import HistoryPage from "../Pages/History/HistoryPage";
-import BigRoomPage from "../Pages/Rooms/BigRoomPage";
 import RoomPage from "../Pages/Rooms/RoomPage";
 
-
 const Routers = () => {
-    return (<Routes>
-        <Route path="/" element={<IsAuthenticated />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
+  return (
+    <Routes>
+      <Route path="/" element={<IsAuthenticated />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
 
-        {/* PROTECTED ROUTES */}
-        <Route path="/overview" element={<ProtectedRoute outlet={<OverviewPage />} />}></Route>
-        {/*<Route path="/bigroom" element={<ProtectedRoute outlet={<BigRoomPage />} />}></Route>*/}
-        <Route path="/rooms/:roomId" element={<ProtectedRoute outlet={<RoomPage />} />}></Route>
-        <Route path="/history" element={<ProtectedRoute outlet={<HistoryPage />} />}></Route>
-    </Routes>)
-}
+      {/* PROTECTED ROUTES */}
+      <Route
+        path="/overview"
+        element={<ProtectedRoute outlet={<OverviewPage />} />}
+      ></Route>
+      <Route
+        path="/rooms/:roomId"
+        element={<ProtectedRoute outlet={<RoomPage />} />}
+      ></Route>
+      <Route
+        path="/history"
+        element={<ProtectedRoute outlet={<HistoryPage />} />}
+      ></Route>
+    </Routes>
+  );
+};
 
 export default Routers;
--- client/src/stores/RoomStore.jsx
+++ client/src/stores/RoomStore.jsx
@@ -1,42 +1,40 @@
-import { makeAutoObservable} from "mobx";
-import {RootStore, useStores} from "./";
+import { makeAutoObservable } from "mobx";
+import { RootStore } from "./";
 import toast from "react-hot-toast";
-import {bigRoomSeats, rooms} from "../data/seats";
-class RoomStore{
-    
-    rooms = [];
-    
-    seats = [];
-    
-    openDialog = false;
-    constructor(rootStore: RootStore) {
-        makeAutoObservable(this);
-    }
+class RoomStore {
+  rooms = [];
 
-    bookSeat(id, isTaken) {
-        const seatToUpdate = this.seats.find(seat => seat.id === id);
+  seats = [];
 
-        if (seatToUpdate) {
-            seatToUpdate.isTaken = isTaken;
-            toast.success('Booked seat')
-        } else {
-            console.log(`Seat with ID ${id} not found.`);
-            toast.error('Seat not found');
-        }
-    }
-    
-    setSeats(chosenSeats) {
-        this.seats = chosenSeats;
-    }
-    
-    /* DIALOG */
-    handleOpenDialog = () => {
-        this.openDialog = !this.openDialog;
-    };
+  openDialog = false;
+  constructor(rootStore: RootStore) {
+    makeAutoObservable(this);
+  }
 
-    handleCloseDialog = () => {
-        this.openDialog = !this.openDialog;
-    };
+  bookSeat(id, isTaken) {
+    const seatToUpdate = this.seats.find((seat) => seat.id === id);
+
+    if (seatToUpdate) {
+      seatToUpdate.isTaken = isTaken;
+      toast.success("Booked seat");
+    } else {
+      console.log(`Seat with ID ${id} not found.`);
+      toast.error("Seat not found");
+    }
+  }
+
+  setSeats(chosenSeats) {
+    this.seats = chosenSeats;
+  }
+
+  /* DIALOG */
+  handleOpenDialog = () => {
+    this.openDialog = !this.openDialog;
+  };
+
+  handleCloseDialog = () => {
+    this.openDialog = !this.openDialog;
+  };
 }
 
 export default RoomStore;
