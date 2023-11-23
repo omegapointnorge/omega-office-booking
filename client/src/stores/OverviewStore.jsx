b34e3eb6229355f6ac34ebcf3d1d5d193e90cf4d
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-11-23 12:48:55 +0100

    Added getRooms handling in OverviewStore

--- client/src/stores/OverviewStore.jsx
+++ client/src/stores/OverviewStore.jsx
@@ -5,9 +5,38 @@ class OverviewStore {
   rooms = [];
 
   constructor() {
+    this.initialize();
     makeAutoObservable(this);
 
     this.rooms = rooms;
+  }
+
+  async initialize() {
+    try {
+      const url = "/api/Room/rooms";
+
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+
+      console.log(response);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      this.setRooms(data); // Update MobX store with the fetched data
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  setRooms(data) {
+    this.rooms = data.value;
   }
 }
 
