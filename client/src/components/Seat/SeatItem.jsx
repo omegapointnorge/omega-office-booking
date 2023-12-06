aee1f5abca133fdaae00a1947598c357ebb26332
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-12-06 13:25:24 +0100

    Fixed SeatItem

--- client/src/components/Seat/SeatItem.jsx
+++ client/src/components/Seat/SeatItem.jsx
@@ -9,19 +9,20 @@ const SeatItem = ({
   top,
 }) => {
   return (
-    <div>
-      <div className={`w-8 h-7  absolute cursor-pointer`} onClick={onClick}>
-        <div
-          className={`w-8 h-5 left-0 top-0 absolute ${
-            !isTaken ? "bg-slate-300" : "bg-orange-400"
-          } rounded-lg`}
-        />
-        <div
-          className={`w-3.5 h-3 left-[9.47px] top-[14.72px] absolute ${
-            !isTaken ? "bg-slate-300" : "bg-orange-400"
-          } rounded-lg`}
-        />
-      </div>
+    <div
+      className="origin-top-left rotate-[-90.62deg] w-14 h-10 relative cursor-pointer"
+      onClick={onClick}
+    >
+      <div
+        className={`w-14 h-8 left-0 top-0 absolute origin-top-left rotate-[-90.62deg] ${
+          !isTaken ? "bg-slate-300" : "bg-orange-400"
+        } rounded-lg`}
+      />
+      <div
+        className={`w-6 h-5 left-[21.82px] top-[-16.24px] absolute origin-top-left rotate-[-90.62deg] ${
+          !isTaken ? "bg-slate-300" : "bg-orange-400"
+        } rounded-lg`}
+      />
     </div>
   );
 };
