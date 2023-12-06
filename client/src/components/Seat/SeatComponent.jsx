235243538d0d4810f2d8551c4cac889005b16844
mostafasaziz <mostafa.aziz@omegapoint.no>
2023-12-06 13:09:59 +0100

    Added custom isTaken

--- client/src/components/Seat/SeatComponent.jsx
+++ client/src/components/Seat/SeatComponent.jsx
@@ -1,4 +1,11 @@
-export const SeatComponent = ({ bottom, right, left, top, rotate }) => {
+export const SeatComponent = ({
+  bottom,
+  right,
+  left,
+  top,
+  rotate,
+  isTaken,
+}) => {
   return (
     <div
       className={`w-8 h-7  ${rotate} ${bottom} ${left ?? null} ${
@@ -6,9 +13,15 @@ export const SeatComponent = ({ bottom, right, left, top, rotate }) => {
       } ${top} absolute`}
     >
       <div
-        className={`w-8 h-5 left-0 top-0 absolute bg-slate-300 rounded-lg`}
+        className={`w-8 h-5 left-0 top-0 absolute ${
+          !isTaken ? "bg-slate-300" : "bg-orange-400"
+        } rounded-lg`}
       />
-      <div className="w-3.5 h-3 left-[9.47px] top-[14.72px] absolute bg-slate-300 rounded-lg" />
+      <div
+        className={`w-3.5 h-3 left-[9.47px] top-[14.72px] absolute ${
+          !isTaken ? "bg-slate-300" : "bg-orange-400"
+        } rounded-lg`}
+      />
     </div>
   );
 };
