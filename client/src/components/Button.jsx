2052a01eb67f96b7be6dc97aebcbf65ce4735407
Synne Kjærvik <synne.bakke@omegapoint.no>
2023-12-07 10:41:52 +0100

    Added marine bg color to button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,10 +1,17 @@
-
-const Button = ({label, onClick, disabled, outline, small, alert, icon:Icon }) =>{
-    return (
-        <button 
-            onClick={onClick}
-            disabled={disabled}
-            className={`
+const Button = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  alert,
+  icon: Icon,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`
       relative
       disabled:opacity-70
       disabled:cursor-not-allowed
@@ -12,6 +19,7 @@ const Button = ({label, onClick, disabled, outline, small, alert, icon:Icon }) =
       hover:opacity-80
       transition
       w-full
+      bg-marine
       ${outline ? "bg-white" : "bg-blue-900"}
       ${outline ? "#D6D6D6" : "border-blue-900"}
       ${outline ? "text-black" : "text-white"}
@@ -21,9 +29,12 @@ const Button = ({label, onClick, disabled, outline, small, alert, icon:Icon }) =
       ${small ? "border-[1px]" : "border-2"}
       ${alert ? "bg-red-800" : "bg-blue-900"}
       ${alert ? "border-red-800" : "border-blue-900"}
-`}> {label}
-            {Icon && <Icon size={24} className="absolute left-4 top-3" />}</button>
-    )
-} 
+`}
+    >
+      {label}
+      {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+    </button>
+  );
+};
 
 export default Button;
