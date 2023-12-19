b37246bdd11edd954831a28456628cd29453f1db
GmailGit <vicky.huangyuanxin@gmail.com>
2023-12-19 08:48:34 +0100

    Add is available  field migration

---
+++ server/Migrations/20231215135220_addNewFieldIsAvailableToSeatEntity.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewFieldIsAvailableToSeatEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Seats",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "IsAvailable",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Seats");
+        }
+    }
+}
