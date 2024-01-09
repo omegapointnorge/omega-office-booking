3f41ca18345f0ac5e3c348fb5d9ad12344f99c2d
Nils Olav Johansen <Nilsokj@gmail.com>
2024-01-09 14:35:13 +0100

    added new history page

--- client/src/Pages/History/HistoryPage.jsx
+++ client/src/Pages/History/HistoryPage.jsx
@@ -1,56 +1,100 @@
+import React, {useEffect, useState} from "react";
 import Heading from "../../components/Heading";
-import { observer } from "mobx-react-lite";
+import {observer} from "mobx-react-lite";
 import BookingItem from "../../components/Bookings/BookingItem";
 import historyStore from "../../stores/HistoryStore";
 import MyDialog from "../../components/Dialog";
-
+import {IoIosArrowBack, IoIosArrowForward} from "react-icons/io";
 
 const HistoryPage = observer(() => {
-  if (historyStore.myBookings.length === 0) {
+    const [isFirstPage, setIsFirstPage] = useState(true);
+const [isLastPage, setIsLastPage] = useState(false);
+
+const navigateNext = () => {
+    if (!isLastPage) {
+        historyStore.navigateNext();
+        setIsFirstPage(false);
+        if (historyStore.pageNumber === historyStore.lastPage) setIsLastPage(true)
+    }
+};
+
+const navigatePrevious = () => {
+    if (!isFirstPage) {
+        historyStore.navigatePrevious();
+        setIsFirstPage(true);
+        if (historyStore.pageNumber !== historyStore.lastPage) setIsLastPage(false)
+    }
+};
+
+    if (historyStore.myUpcomingBookings.length === 0) {
+        return (
+            <>
+                <div
+                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto
+     fixed inset-0 outline-none focus:outline-none"
+                >
+                    <Heading title="You dont have any bookings"/>
+                </div>
+            </>
+        );
+    }
     return (
-      <>
-        <div
-          className="justify-center items-center flex overflow-x-hidden overflow-y-auto 
-     fixed inset-0 outline-none focus:outline-none"
-        >
-          <Heading title="You dont have any bookings" />
-        </div>
-      </>
+        <>
+            <div
+                className="justify-center items-center flex flex-col fixed inset-0 mt-10"
+            >
+                <Heading
+                    title="Your bookings"
+                />
+                <div className="container mt-3">
+                    <div className="flex flex-col gap-4 mb-10">
+                        <p className="text-left text-xl font-semibold heading mb-3">UPCOMING BOOKINGS</p>
+                        <div className="flex flex-row gap-5">
+                            {historyStore.myUpcomingBookings
+                                .map((booking) => (
+                                    <BookingItem
+                                        key={booking.id}
+                                        roomName={booking.roomName}
+                                        seatId={booking.seatId}
+                                        bookingDateTime={booking.bookingDateTime}
+                                        showDeleteButton={true}
+                                        onClick={() => {
+                                            historyStore.handleOpenDialog(booking.id);
+                                        }}
+                                    ></BookingItem>
+                                ))}
+                            <MyDialog
+                                title="Delete your Seat?"
+                                open={historyStore.openDialog}
+                                handleClose={historyStore.handleCloseDialog}
+                                onClick={() => {
+                                    historyStore.deleteBooking(historyStore.bookingIdToDelete);
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <p className="text-left text-xl font-semibold heading mb-3">PREVIOUS BOOKINGS</p>
+                        <div className="flex flex-row gap-5">
+                            <button onClick={() => navigatePrevious()} className={`${isFirstPage ? 'opacity-0' : 'opacity-100'}`} disabled={isFirstPage}>
+                                <IoIosArrowBack/>
+                            </button>
+                            {historyStore.myPreviousBookings
+                                .map((booking) => (
+                                    <BookingItem key={booking.id} roomName={booking.roomName}
+                                                 seatId={booking.seatId}
+                                                 bookingDateTime={booking.bookingDateTime} showDeleteButton={false}
+                                    ></BookingItem>
+                                ))}
+                            <button onClick={() => navigateNext()} className={`${isLastPage ? 'opacity-0' : 'opacity-100'}`} disabled={isLastPage}>
+                                <IoIosArrowForward />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
-  }
-
-  return (
-    <>
-      <div
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto 
-     fixed inset-0 outline-none focus:outline-none"
-      >
-        <div className="flex flex-col gap-10">
-          <Heading
-            title="Your bookings"
-            subTitle="A summary of your bookings"
-          />
-          {historyStore.myBookings.map((booking) => (
-            <BookingItem key={booking.id} seatId={booking.seatId} name={booking.dateTime}
-              onClick={() => {
-                historyStore.handleOpenDialog(booking.id);
-              }}></BookingItem>
-
-          ))}
-          <MyDialog
-            title="Delete your Seat?"
-            open={historyStore.openDialog}
-            handleClose={historyStore.handleCloseDialog}
-            onClick={() => {
-              historyStore.deleteBooking(historyStore.bookingIdToDelete);
-            }}
-          />
-
-
-        </div>
-      </div>
-    </>
-  );
 });
 
 export default HistoryPage;
--- client/src/components/Bookings/BookingItem.jsx
+++ client/src/components/Bookings/BookingItem.jsx
@@ -1,33 +1,28 @@
 import { MdDelete } from "react-icons/md";
 
-const BookingItem = ({ onClick, dateTime, roomName, seatId, name }) => {
+const BookingItem = ({ onClick, roomName, bookingDateTime, seatId, showDeleteButton }) => {
   return (
-    <ul className="divide-y divide-gray-100 p-4 rounded-[24px] bg-cyan-50">
-      <li className="flex justify-between gap-x-6 py-5">
-        <div className="flex min-w-0 gap-x-4">
-          <div className="min-w-0 flex-auto">
-            <p className="text-sm font-semibold leading-6 text-gray-900">
-              {name}
-            </p>
-            <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-              Sete: {seatId}
-            </p>
+      <ul className="divide-y divide-gray-100 p-4 rounded-[24px] bg-white">
+        <li className="flex justify-between gap-x-6 py-5">
+          <div className="flex min-w-0 gap-x-4">
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                {bookingDateTime}
+              </p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                {roomName}Romnavn, sete {seatId}
+              </p>
+            </div>
+                <MdDelete
+                    onClick={onClick}
+                    className={`h-10 w-10 flex-none text-black hover:text-red-500 transition-colors duration-200 ${!showDeleteButton ? 'opacity-0' : 'opacity-100'}`}
+                    src=""
+                    alt=""
+                    disabled={!showDeleteButton}
+                />
           </div>
-        </div>
-        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-          <p className="text-sm leading-6 text-gray-900">{dateTime}</p>
-          <p className="mt-1 text-xs leading-5 text-gray-500">{seatId}</p>
-        </div>
-        <MdDelete
-          onClick={onClick}
-          color="red"
-          className="h-12 w-12 flex-none bg-gray-50 hover:bg-red-100 transition-colors duration-300"
-          src=""
-          alt=""
-        />
-      </li>
-    </ul>
-
+        </li>
+      </ul>
   );
 };
 
