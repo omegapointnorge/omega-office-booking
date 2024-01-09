fb50ee0a9ae056c8c538a3e7d65f39689cf084d1
mbsmebye <mbsmebye@gmail.com>
2024-01-09 15:59:25 +0100

    Adjust SeatInfoModal for functional delete-function

--- client/src/Pages/Overview/SeatInfoModal.jsx
+++ client/src/Pages/Overview/SeatInfoModal.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { observer } from 'mobx-react-lite';
 import bookingStore from '../../stores/BookingStore';
-import { BookingRequest } from '../../domain/booking';
+import Booking, { CreateBookingRequest, DeleteBookingRequest } from '../../domain/booking';
 import { useAuthContext } from '../../api/useAuthContext';
 
 
@@ -14,15 +14,13 @@ const SeatInfoModal = observer(({ onClose, selectedSeatId }) => {
     const { activeBookings } = bookingStore;
 
     const [bookingDateTime, setBookingDateTime] = useState(initialDateTime);
-    const [bookedBy, setBookedBy] = useState();
+    const [selectedBooking, setSelectedBooking] = useState(new Booking());
 
     useEffect(() => {
         const foundBooking = activeBookings.find(booking => booking.seatId === selectedSeatId);
         if (foundBooking) {
-            setBookedBy(foundBooking.userId);
+            setSelectedBooking(foundBooking)
             setBookingDateTime(new Date(foundBooking.bookingDateTime).toISOString().slice(0, 16));
-        } else {
-            setBookedBy('');
         }
     }, [selectedSeatId, activeBookings]);
 
@@ -31,17 +29,19 @@ const SeatInfoModal = observer(({ onClose, selectedSeatId }) => {
     };
 
     const handleBook = async () => {
-        const booking = new BookingRequest(selectedSeatId);
-        await bookingStore.createBooking(booking);
+        const createBookingRequest = new CreateBookingRequest(selectedSeatId);
+        await bookingStore.createBooking(createBookingRequest);
+        onClose()
+    };
+    
+    const handleDelete = async () => {
+        const deleteBookingRequest = new DeleteBookingRequest(selectedBooking.id)
+        await bookingStore.deleteBooking(deleteBookingRequest);
         onClose()
     };
 
-    const handleDelete = () => {
-        console.log("Booking seat:", selectedSeatId);
-    };
-
     const getButtonGroup = () => {
-        if (bookedBy && userId !== bookedBy) {
+        if (selectedBooking.id && userId !== selectedBooking.userId) {
             return (
                 <button onClick={onClose} className="mt-5 px-5 py-2 bg-blue-600 text-white text-sm font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                 Close
@@ -54,12 +54,12 @@ const SeatInfoModal = observer(({ onClose, selectedSeatId }) => {
                     <button onClick={onClose} className="px-5 py-2 bg-blue-600 text-white text-sm font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
                         Close
                     </button>
-                    {(userId === bookedBy) &&
+                    {(userId === selectedBooking.userId) &&
                     <button onClick={handleDelete} className="px-5 py-2 bg-red-600 text-white text-sm font-medium rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
                         Slett reservasjon
                     </button>
                     }
-                    {(bookedBy === "") &&
+                    {(selectedBooking.id === null) &&
                         <button onClick={handleBook} className="px-5 py-2 bg-green-600 text-white text-sm font-medium rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">
                         Reserver sete
                     </button>
@@ -78,7 +78,7 @@ const SeatInfoModal = observer(({ onClose, selectedSeatId }) => {
             <h3 className="text-xl font-semibold text-gray-900 mb-4">Seat Information</h3>
             <div className="text-left space-y-3">
               <p className="text-sm text-gray-600">Seat ID: <span className="text-gray-700 font-medium">{selectedSeatId}</span></p>
-              <p className="text-sm text-gray-600">Booked by: <span className="text-gray-700 font-medium">{bookedBy || 'Not booked'}</span></p>
+              <p className="text-sm text-gray-600">Booked by: <span className="text-gray-700 font-medium">{selectedBooking.id || 'Not booked'}</span></p>
               <div className="flex items-center text-sm text-gray-600 space-x-2">
                 <label htmlFor="booking-date" className="font-medium">Booking Date:</label>
                 <input 
