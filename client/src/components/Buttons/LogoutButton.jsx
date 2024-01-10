313c1db1621d4b48c4e8b77dc30b5a83a635665e
Jenny Lofthus <lofthus.jenny@gmail.com>
2024-01-10 13:24:12 +0100

    fix navbar blocking content

--- client/src/components/Buttons/LogoutButton.jsx
+++ client/src/components/Buttons/LogoutButton.jsx
@@ -1,17 +1,15 @@
 import {Link} from "react-router-dom";
-import Button from "../Button";
-
 
 export const LogoutButton = () => {
     const LogoutUrl = "/api/Account/Logout";
     
     return <div>
         <Link
-            className="flex flex-row items-center gap-4 w-full z-5000"
+            className="text-lg flex flex-row items-center font-semibold gap-4 w-full z-5000"
             to={LogoutUrl}
             reloadDocument
         >
-            <Button alert label="Logout" />
+            Logout
         </Link>
     </div>
 }
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,24 +7,32 @@ export const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed w-full bg-warmgray z-10 shadow-sm ">
+    <div className="w-full bg-warmgray z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0 cursor-pointer">
-            <Logo />
-            <NavbarItem
-              label="Overview"
-              onClick={() => {
-                navigate("/overview");
-              }}
-            />
-            <NavbarItem
-              label="History"
-              onClick={() => {
-                navigate("/history");
-              }}
-            />
-            <LogoutButton />
+              <ul className="flex items-center justify-between gap-10">
+                  <li>
+                      <Logo/>
+                  </li>
+                  <li>
+                      <NavbarItem
+                          label="Overview"
+                          onClick={() => {
+                              navigate("/overview");
+                          }}
+                      />
+                  </li>
+                  <li>
+                      <NavbarItem
+                          label="History"
+                          onClick={() => {
+                              navigate("/history");
+                          }}
+                      />
+                  </li>
+              </ul>
+              <LogoutButton />
           </div>
         </div>
       </div>
