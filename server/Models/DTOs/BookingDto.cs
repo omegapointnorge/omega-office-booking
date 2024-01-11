d9575adb6fc95e3518efa22c10241cacdc5d7e0b
mbsmebye <mbsmebye@gmail.com>
2024-01-11 10:41:03 +0100

    Remove unused constructor

--- server/Models/DTOs/BookingDto.cs
+++ server/Models/DTOs/BookingDto.cs
@@ -9,15 +9,6 @@ namespace server.Models.DTOs
         public int SeatId { get; set; }
         public String BookingDateTime { get; set; }
 
-        public BookingDto(int id, String userId, int seatId, DateTime dateTime)
-        {
-            // Convert to Norwegian time zone
-            Id = id;
-            UserId = userId;
-            SeatId = seatId;
-            BookingDateTime = dateTime.ToUniversalTime().ToString("o");
-        }
-
         public BookingDto(Booking booking) {
             Id = booking.Id;
             UserId = booking.UserId;
