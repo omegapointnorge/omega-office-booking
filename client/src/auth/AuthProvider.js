e90aaf06217c73364879d93024fce33bf0bfe96a
Nils Olav Johansen <Nilsokj@gmail.com>
2024-01-11 12:48:45 +0100

    added loading while waiting for context to be fetched

--- client/src/auth/AuthProvider.js
+++ client/src/auth/AuthProvider.js
@@ -3,14 +3,16 @@ import { getUser } from '../api/userService.js'
 import AuthContext from './AuthContext'
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     
     useEffect(() => {
         getUser()
             .then(response => { setUser(response) })
             .catch(e => setUser({ isAuthenticated: false }))
+            .finally(() => setLoading(false));
     }, []);
 
     return (
-        <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{ user, loading }}>{children}</AuthContext.Provider>
     );
 };
