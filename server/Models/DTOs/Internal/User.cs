94b19c5a5d58219dcefb4e7bf4b601ff6f524dc2
GmailGit <vicky.huangyuanxin@gmail.com>
2024-01-16 14:25:05 +0100

    Change class User to record, we just need to presend built in value

--- server/Models/DTOs/Internal/User.cs
+++ server/Models/DTOs/Internal/User.cs
@@ -2,7 +2,7 @@
 
 namespace server.Models.DTOs.Internal
 {
-    public class User
+    public record User
     {
         public string UserName { get; set; }
         public string UserId { get; set; }
