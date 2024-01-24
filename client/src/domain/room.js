ddd90e7a9af00d2b81e620a8ab38059350a5d6e8
mbsmebye <mbsmebye@gmail.com>
2024-01-24 12:22:46 +0100

    Moved room and seat classes to own files

--- client/src/domain/room.js
+++ client/src/domain/room.js
@@ -1,7 +1,7 @@
-export class Room {
-  constructor(id, name, seats) {
-    this.id = id;
-    this.name = name;
-    this.seats = seats;
+export default class Room {
+  constructor(id = null, name = null, seats = []) {
+      this.id = id;
+      this.name = name;
+      this.seats = seats.map(seat => new Seat(seat.id, seat.roomId, seat.isAvailable, seat.bookings));
   }
 }
