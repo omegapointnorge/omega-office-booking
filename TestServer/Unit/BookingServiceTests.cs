6d660b9c126171b829a73d23f112ba2ae20e807b
Nils Olav Kvelvane Johansen <nils.olav.johansen@itverket.no>
2024-02-15 13:57:11 +0100

    wrote tests for booking before and after 15

--- TestServer/Unit/BookingServiceTests.cs
+++ TestServer/Unit/BookingServiceTests.cs
@@ -1,4 +1,5 @@
 using Moq;
+using server.Helpers;
 using server.Models.Domain;
 using server.Models.DTOs.Internal;
 using server.Models.DTOs.Request;
@@ -30,17 +31,11 @@ namespace server.Services.Internal.Tests
             return new UserClaims("name", "testUser", "EventAdmin");
         }
 
-
-        private CreateBookingRequest GetBookingRequest()
-        {
-            return new CreateBookingRequest { BookingDateTime = DateTime.Now, SeatId = 1 };
-        }
-
         [Fact]
         public async Task CreateBookingAsync_ValidBooking_ReturnsBookingDto()
         {
             // Arrange
-            var bookingRequest = GetBookingRequest();
+            var bookingRequest = new CreateBookingRequest { BookingDateTime = DateTime.Now, SeatId = 1 };
             var userClaims = GetUserClaims();
 
             _bookingRepositoryMock.Setup(repo => repo.GetAsync()).ReturnsAsync(new List<Booking>());
@@ -61,7 +56,7 @@ namespace server.Services.Internal.Tests
         public async Task CreateBookingAsync_SeatAlreadyBooked_ThrowsException()
         {
             // Arrange
-            var bookingRequest = GetBookingRequest();
+            var bookingRequest = new CreateBookingRequest { BookingDateTime = DateTime.Now, SeatId = 1 };
             var userClaims = GetUserClaims();
             _bookingRepositoryMock.Setup(repo => repo.GetAsync()).ReturnsAsync(new List<Booking> { new Booking { SeatId = bookingRequest.SeatId, BookingDateTime = bookingRequest.BookingDateTime } });
 
@@ -73,7 +68,7 @@ namespace server.Services.Internal.Tests
         public async Task CreateBookingAsync_UserAlreadyBookedForDay_ThrowsException()
         {
             // Arrange
-            var bookingRequest = GetBookingRequest();
+            var bookingRequest = new CreateBookingRequest { BookingDateTime = DateTime.Now, SeatId = 1 };
             var userClaims = GetUserClaims();
             _bookingRepositoryMock.Setup(repo => repo.GetAsync()).ReturnsAsync(new List<Booking> { new Booking { UserId = userClaims.Objectidentifier, BookingDateTime = bookingRequest.BookingDateTime } });
 
@@ -98,6 +93,41 @@ namespace server.Services.Internal.Tests
             Assert.NotNull(result);
             Assert.Equal(adminClaims.Objectidentifier, result.UserId);
             Assert.Equal(adminClaims.UserName, result.UserName);
+            Assert.Equal(bookingRequest.SeatId, result.SeatId);
+            Assert.Equal(bookingRequest.BookingDateTime.ToUniversalTime().ToString("o"), result.BookingDateTime);
+        }
+
+        [Fact]
+        public async Task CreateBookingAsync_AttemptToBookBefore15_ThrowsException()
+        {
+            // Arrange
+            var bookingDateTime = DateTime.Today.AddDays(1).AddHours(14); // Next day after 14:00
+            var bookingRequest = new CreateBookingRequest { BookingDateTime = bookingDateTime, SeatId = 2 };
+            var userClaims = GetUserClaims();
+
+            _bookingRepositoryMock.Setup(repo => repo.GetAsync()).ReturnsAsync(new List<Booking>());
+
+            // Act & Assert
+            await Assert.ThrowsAsync<Exception>(() => Sut.CreateBookingAsync(bookingRequest, userClaims));
+        }
+
+        [Fact]
+        public async Task CreateBookingAsync_AdminBookingBefore15_ReturnsBookingDto()
+        {
+            // Arrange
+            var bookingDateTime = DateTime.Today.AddDays(1).AddHours(14); // Next day after 14:00
+            var bookingRequest = new CreateBookingRequest { BookingDateTime = DateTime.Now, SeatId = 2 };
+            var userClaims = GetEventAdminClaims();
+
+            _bookingRepositoryMock.Setup(repo => repo.GetAsync()).ReturnsAsync(new List<Booking>());
+
+            // Act
+            var result = await Sut.CreateBookingAsync(bookingRequest, userClaims);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(userClaims.Objectidentifier, result.UserId);
+            Assert.Equal(userClaims.UserName, result.UserName);
             Assert.Equal(bookingRequest.SeatId, result.SeatId);
             Assert.Equal(bookingRequest.BookingDateTime.ToUniversalTime().ToString("o"), result.BookingDateTime);
         }
