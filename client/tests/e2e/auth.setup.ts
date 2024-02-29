f4be90da6041262819c426e027d26bb20e96a8a2
HMS <hiruthstautland@gmail.com>
2024-02-29 11:07:20 +0100

    authentication draft

--- client/tests/e2e/auth.setup.ts
+++ client/tests/e2e/auth.setup.ts
@@ -4,10 +4,9 @@ const authFile = "playwright/.auth/user.json";
 
 setup("authenticate", async ({ page }) => {
   // Perform authentication steps. Replace these actions with your own.
-  await page.goto("https://github.com/login");
-  await page.getByLabel("Username or email address").fill("username");
-  await page.getByLabel("Password").fill("password");
-  await page.getByRole("button", { name: "Sign in" }).click();
+   await page.goto("https://officebooking.omegapoint.no");
+   await page.getByLabel("Password").fill("password");
+  await page.getByRole("button", { name: "Logg inn" }).click();
   // Wait until the page receives the cookies.
   //
   // Sometimes login flow sets cookies in the process of several redirects.
