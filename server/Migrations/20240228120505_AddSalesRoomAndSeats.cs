a65f3b65ba7982071a577167b1f38cd6e621bd0d
mbsmebye <mbsmebye@gmail.com>
2024-02-29 12:10:07 +0100

    Update DB to include new rooms

---
+++ server/Migrations/20240228120505_AddSalesRoomAndSeats.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSalesRoomAndSeats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Salg" });
+
+            migrationBuilder.InsertData(
+                table: "Seats",
+                columns: new[] { "Id", "IsAvailable", "RoomId" },
+                values: new object[,]
+                {
+                    { 16, true, 3 },
+                    { 17, true, 3 },
+                    { 18, true, 3 },
+                    { 19, true, 3 },
+                    { 20, true, 3 },
+                    { 21, true, 3 },
+                    { 22, true, 3 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Seats",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
