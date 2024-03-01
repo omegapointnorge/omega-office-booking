8122f849bc0f22ff4d61dcfadc2977723a96f9c5
Vicky(Yuanxin) Huang <vicky.huangyuanxin@gmail.com>
2024-03-01 14:48:50 +0100

    change the url to baseUrl

--- client/tests/officebooking.spec.ts
+++ client/tests/officebooking.spec.ts
@@ -2,7 +2,7 @@ import { test , expect } from "@playwright/test";
 
 test.describe('Load logg inn page', () => {
   test('has title', async ({ page }) => {
-    await page.goto('https://app-officebooking.azurewebsites.net/');
+    await page.goto('/');
 
     // Expect a title "Sign in to your account" when no authorization.
     await expect(page).toHaveTitle(/Sign in to your account/);
@@ -10,7 +10,7 @@ test.describe('Load logg inn page', () => {
 });
 test.describe('/getUser endpoint', () => {
   test('GET - current user', async ({ request }) => {
-    const response = await request.get('https://app-officebooking.azurewebsites.net/client/User');
+    const response = await request.get('/client/User');
     await expect(response).toBeOK();
 
   });
