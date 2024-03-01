e637e94dbb93c3bf76f5eb5a734e1a3fd3cb18dc
Nils Olav Kvelvane Johansen <nils.olav.johansen@itverket.no>
2024-03-01 13:01:30 +0100

    added endpoint to fetch unavailable seats

---
+++ server/Repository/SeatRepository.cs
@@ -0,0 +1,11 @@
+﻿using server.Context;
+using server.Models.Domain;
+
+namespace server.Repository;
+
+public class SeatRepository : Repository<Seat>, ISeatRepository
+{
+    public SeatRepository(OfficeDbContext context) : base(context)
+    {
+    }
+}
