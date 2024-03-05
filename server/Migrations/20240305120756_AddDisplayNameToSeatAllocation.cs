468458ede59f59eadf6b499e99f7d6946e284152
Nils Olav Kvelvane Johansen <nils.olav.johansen@itverket.no>
2024-03-05 14:54:35 +0100

    add migrations

---
+++ server/Migrations/20240305120756_AddDisplayNameToSeatAllocation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayNameToSeatAllocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "SeatAllocation",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "SeatAllocation",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DisplayName", "UserId" },
+                values: new object[] { "Fornavn Etternavn", "7534c00c-79ee-4230-b721-8cd781c2212d" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "SeatAllocation");
+
+            migrationBuilder.UpdateData(
+                table: "SeatAllocation",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserId",
+                value: "Salg test Johansen");
+        }
+    }
+}
