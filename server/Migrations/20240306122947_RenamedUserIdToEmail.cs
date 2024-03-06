f1d26b26a547a706acf722e55c594c14ba34192c
Nils Olav Kvelvane Johansen <nils.olav.johansen@itverket.no>
2024-03-06 13:30:53 +0100

    added new migration

---
+++ server/Migrations/20240306122947_RenamedUserIdToEmail.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedUserIdToEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "SeatAllocation",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "SeatAllocation",
+                newName: "UserId");
+        }
+    }
+}
