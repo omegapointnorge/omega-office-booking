8c47bce515e68d1f97119091b2a13e71a6cf8ae2
Nils Olav Kvelvane Johansen <nils.olav.johansen@itverket.no>
2024-03-08 10:43:19 +0100

    added migration

---
+++ server/Migrations/20240308092938_ChangedSeatAllocation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedSeatAllocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SeatAllocation",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "SeatAllocation",
+                columns: new[] { "Id", "Email", "SeatId" },
+                values: new object[] { 1, "test@email.no", 2 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SeatAllocation",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "SeatAllocation",
+                columns: new[] { "Id", "Email", "SeatId" },
+                values: new object[] { 1, "Salg test Johansen", 1 });
+        }
+    }
+}
