58d9e2cd665d78e766af9966adaa9221240a7b53
Nils Olav Kvelvane Johansen <nils.olav.johansen@itverket.no>
2024-03-14 12:35:35 +0100

    added migration

---
+++ server/Migrations/20240314112035_MergedSeatAndSeatAllocation.cs
@@ -0,0 +1,440 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MergedSeatAndSeatAllocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SeatAllocation");
+
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Seat");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeatOwnerEmail",
+                table: "Seat",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "SeatOwnerEmail",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "SeatOwnerEmail",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeatOwnerEmail",
+                table: "Seat");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Seat",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "SeatAllocation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SeatId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeatAllocation", x => x.Id);
+                    table.UniqueConstraint("unique_seat_constraint", x => x.SeatId);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "IsAvailable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "IsAvailable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "IsAvailable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "IsAvailable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "IsAvailable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "IsAvailable",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 23,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 24,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 25,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 26,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Seat",
+                keyColumn: "Id",
+                keyValue: 27,
+                column: "IsAvailable",
+                value: true);
+
+            migrationBuilder.InsertData(
+                table: "SeatAllocation",
+                columns: new[] { "Id", "Email", "SeatId" },
+                values: new object[] { 1, "test@email.no", 2 });
+        }
+    }
+}
