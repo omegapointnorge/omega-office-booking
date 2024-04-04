640cc24933ffc0ef10f9fc5123057ab2da00275a
Rosita Høybakken <rh@itverket.no>
2024-04-04 15:25:54 +0200

    add migration to remove sales room from room table and all seats in the sales room

---
+++ server/Migrations/20240404111556_RemoveSalesRoomAndSeats.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSalesRoomAndSeats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DeleteData(
+                    table: "Seat",
+                    keyColumn: "RoomId",
+                    keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Room",
+                columns: new [] { "Id", "Name" },
+                values: new object[] {3, "Salg"});
+
+            migrationBuilder.InsertData(
+                table: "Seat",
+                columns: new[] { "Id", "IsAvailable", "RoomId" },
+                values: new object[,]
+                {
+                    { 16, false, 3 },
+                    { 17, true, 3 },
+                    { 18, false, 3 },
+                    { 19, false, 3 },
+                    { 20, false, 3 },
+                    { 21, false, 3 },
+                    { 22, false, 3 }
+                });
+        }
+    }
+}
