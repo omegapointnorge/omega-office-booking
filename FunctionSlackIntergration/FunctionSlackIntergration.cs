9725e3f972770235c1ce648a1bf823f0b006d9f2
Vicky(Yuanxin) Huang <vicky.huangyuanxin@gmail.com>
2024-04-10 12:04:08 +0200

    change time trigger

--- FunctionSlackIntergration/FunctionSlackIntergration.cs
+++ FunctionSlackIntergration/FunctionSlackIntergration.cs
@@ -13,23 +13,22 @@ namespace FunctionSlackIntergration
     public static class FunctionSlackIntergration
     {
         [FunctionName("FunctionSlackIntergration")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task RunAsync(
+        [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+        ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name = "you name";
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
 }
