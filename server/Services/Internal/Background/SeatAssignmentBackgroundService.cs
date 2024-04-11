cd02190e4799401a327b515f4417e372a421f44a
Vicky(Yuanxin) Huang <vicky.huangyuanxin@gmail.com>
2024-04-11 12:20:21 +0200

    commit

--- server/Services/Internal/Background/SeatAssignmentBackgroundService.cs
+++ server/Services/Internal/Background/SeatAssignmentBackgroundService.cs
@@ -84,7 +84,7 @@ public class SeatAssignmentBackgroundService : BackgroundService
         //runs Every day
         _timerHandleSeatAllocation = new Timer(HandleSeatAllocation, null, dueTime, TimeSpan.FromDays(1));
         //only run on deployment
-        _timerInitiateSeatAllocation = new Timer(InitiateSeatAllocation, null, dueTime, Timeout.InfiniteTimeSpan);
+        _timerInitiateSeatAllocation = new Timer(InitiateSeatAllocation, null, TimeSpan.Zero, Timeout.InfiniteTimeSpan);
 
         return Task.CompletedTask;
     }
