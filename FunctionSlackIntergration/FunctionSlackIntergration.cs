049137c073ec55d5b8bbcc463bdae1423923a7fe
Vicky(Yuanxin) Huang <vicky.huangyuanxin@gmail.com>
2024-04-12 09:28:15 +0200

    Revert "add call to channel"
    This reverts commit 2b72dc1794b8957a330aa68e72dce856212e07a2.

--- FunctionSlackIntergration/FunctionSlackIntergration.cs
+++ FunctionSlackIntergration/FunctionSlackIntergration.cs
@@ -7,34 +7,28 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Threading;
-using System.Net.Http;
-using System.Collections.Generic;
 
 namespace FunctionSlackIntergration
 {
     public static class FunctionSlackIntergration
     {
-        private static readonly HttpClient client = new HttpClient();
         [FunctionName("FunctionSlackIntergration")]
-        public static async Task<IActionResult> RunAsync([TimerTrigger("0 */2 * * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log)
+        public static async Task RunAsync(
+        [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+        ILogger log)
         {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = "you name";
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-            Thread.Sleep(10000); // Th
-            var values = new Dictionary<string, string>
-                  {
-                      { "text", "Hello, Call from Function app."},
-                      { "channel", "alert-office-booking" }
-                  };
-
-            var content = new FormUrlEncodedContent(values);
-
-            var response = await client.PostAsync("https://hooks.slack.com/services/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", content);
-
-            var responseString = await response.Content.ReadAsStringAsync();
-            log.LogInformation($"2  C# Timer trigger function executed at: {DateTime.Now}");
-            return new OkObjectResult(responseString);
         }
     }
 }
